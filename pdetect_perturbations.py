c65a99548c386e8eec573a250ef78578ccb68fa4
Dustin Johnson <dustin@thedustyengineer.com>
2020-03-16 23:55:21 -0700

    Initial commit with Perturbations class defined.

---
+++ pdetect_perturbations.py
@@ -0,0 +1,16 @@
+import numpy
+import cv2
+
+class Perturbations:
+
+    @staticmethod
+    def rotate_image(image, rot_val):
+        return
+
+    @staticmethod
+    def adjust_brightness(image, brightness):
+        return
+
+    @staticmethod
+    def add_random_noise(image, noise):
+        return
